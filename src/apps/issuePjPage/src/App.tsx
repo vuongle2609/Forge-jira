06b91a4c06f4bb85bf113de2855d020fa7219095
vuongle2609 <vuongle2609@gmail.com>
2022-06-16 17:44:24 +0700

    import file

--- src/apps/issuePjPage/src/App.tsx
+++ src/apps/issuePjPage/src/App.tsx
@@ -3,117 +3,47 @@ import { requestJira, invoke } from "@forge/bridge";
 import Papa from "papaparse";
 
 function App() {
-  const handleCreateIssues = (dataArray: any[]) => {
+  const handleCreateIssues = async (dataArray: any[]) => {
     const dataArrayModified = dataArray;
     dataArrayModified.pop().shift();
 
-    if (dataArrayModified.length !== 0) {
-      dataArrayModified.forEach((itemArr) => {
-        // console.log(itemArr);
-        const objData = `{
-          "update": {},
-          "fields": {
-            "summary": "Main order flow broken",
-            "parent": {
-              "key": "PROJ-123"
+    const chunkSize = 49;
+    for (let i = 0; i < dataArrayModified.length; i += chunkSize) {
+      const chunk = dataArrayModified.slice(i, i + chunkSize);
+      const objData: any = {
+        issueUpdates: [],
+      };
+
+      chunk.forEach((itemArr) => {
+        objData.issueUpdates.push({
+          update: {},
+          fields: {
+            summary: itemArr[2],
+            issuetype: {
+              id: 10005,
             },
-            "issuetype": {
-              "id": "10000"
+            priority: {
+              id: "1",
             },
-            "components": [{
-              "id": "10000"
-            }],
-            "customfield_20000": "06/Jul/19 3:25 PM",
-            "customfield_40000": {
-              "type": "doc",
-              "version": 1,
-              "content": [{
-                "type": "paragraph",
-                "content": [{
-                  "text": "Occurs on all orders",
-                  "type": "text"
-                }]
-              }]
+            project: {
+              id: 10003,
             },
-            "customfield_70000": [
-              "jira-administrators",
-              "jira-software-users"
-            ],
-            "project": {
-              "id": "10000"
-            },
-            "description": {
-              "type": "doc",
-              "version": 1,
-              "content": [{
-                "type": "paragraph",
-                "content": [{
-                  "text": "Order entry fails when selecting supplier.",
-                  "type": "text"
-                }]
-              }]
-            },
-            "reporter": {
-              "id": "5b10a2844c20165700ede21g"
-            },
-            "fixVersions": [{
-              "id": "10001"
-            }],
-            "customfield_10000": "09/Jun/19",
-            "priority": {
-              "id": "20000"
-            },
-            "labels": [
-              "bugfix",
-              "blitz_test"
-            ],
-            "timetracking": {
-              "remainingEstimate": "5",
-              "originalEstimate": "10"
-            },
-            "customfield_30000": [
-              "10000",
-              "10002"
-            ],
-            "customfield_80000": {
-              "value": "red"
-            },
-            "security": {
-              "id": "10000"
-            },
-            "environment": {
-              "type": "doc",
-              "version": 1,
-              "content": [{
-                "type": "paragraph",
-                "content": [{
-                  "text": "UAT",
-                  "type": "text"
-                }]
-              }]
-            },
-            "versions": [{
-              "id": "10000"
-            }],
-            "duedate": "2019-05-11",
-            "customfield_60000": "jira-software-users",
-            "customfield_50000": {
-              "type": "doc",
-              "version": 1,
-              "content": [{
-                "type": "paragraph",
-                "content": [{
-                  "text": "Could impact day-to-day work.",
-                  "type": "text"
-                }]
-              }]
-            },
-            "assignee": {
-              "id": "5b109f2e9729b51b54dc274d"
-            }
-          }
-        }`;
+            // assignee: {
+
+            // }
+          },
+        });
       });
+
+      const response = await requestJira("/rest/api/3/issue/bulk", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(objData),
+      });
+      console.log(await response.text());
     }
   };
 
@@ -128,122 +58,6 @@ function App() {
     }
   };
 
-  const setUp = async () => {
-    const objData = `{
-      "update": {},
-      "fields": {
-        "summary": "Main order flow broken",
-        "parent": {
-          "key": "PROJ-123"
-        },
-        "issuetype": {
-          "id": "10000"
-        },
-        "components": [{
-          "id": "10000"
-        }],
-        "customfield_20000": "06/Jul/19 3:25 PM",
-        "customfield_40000": {
-          "type": "doc",
-          "version": 1,
-          "content": [{
-            "type": "paragraph",
-            "content": [{
-              "text": "Occurs on all orders",
-              "type": "text"
-            }]
-          }]
-        },
-        "customfield_70000": [
-          "jira-administrators",
-          "jira-software-users"
-        ],
-        "project": {
-          "id": "10000"
-        },
-        "description": {
-          "type": "doc",
-          "version": 1,
-          "content": [{
-            "type": "paragraph",
-            "content": [{
-              "text": "Order entry fails when selecting supplier.",
-              "type": "text"
-            }]
-          }]
-        },
-        "reporter": {
-          "id": "5b10a2844c20165700ede21g"
-        },
-        "fixVersions": [{
-          "id": "10001"
-        }],
-        "customfield_10000": "09/Jun/19",
-        "priority": {
-          "id": "20000"
-        },
-        "labels": [
-          "bugfix",
-          "blitz_test"
-        ],
-        "timetracking": {
-          "remainingEstimate": "5",
-          "originalEstimate": "10"
-        },
-        "customfield_30000": [
-          "10000",
-          "10002"
-        ],
-        "customfield_80000": {
-          "value": "red"
-        },
-        "security": {
-          "id": "10000"
-        },
-        "environment": {
-          "type": "doc",
-          "version": 1,
-          "content": [{
-            "type": "paragraph",
-            "content": [{
-              "text": "UAT",
-              "type": "text"
-            }]
-          }]
-        },
-        "versions": [{
-          "id": "10000"
-        }],
-        "duedate": "2019-05-11",
-        "customfield_60000": "jira-software-users",
-        "customfield_50000": {
-          "type": "doc",
-          "version": 1,
-          "content": [{
-            "type": "paragraph",
-            "content": [{
-              "text": "Could impact day-to-day work.",
-              "type": "text"
-            }]
-          }]
-        },
-        "assignee": {
-          "id": "5b109f2e9729b51b54dc274d"
-        }
-      }
-    }`;
-
-    const response = await requestJira("/rest/api/3/issue", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: objData,
-    });
-    console.log(await response.text());
-  };
-
   return (
     <div className="App">
       <input
@@ -253,7 +67,7 @@ function App() {
         accept=".csv,.xlsx,.xls"
         onChange={getFile}
       />
-      <button onClick={setUp}>kladalsjdlk</button>
+      {/* <button onClick={setUp}>kladalsjdlk</button> */}
     </div>
   );
 }
